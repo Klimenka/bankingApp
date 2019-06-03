1eb85a37904bda1b63ac9eb11148704824a55d9b
AliYussef <36632054+aliyussef@users.noreply.github.com>
2019-06-03 20:48:30 +0200

    Adding relationship to Account class. Adding testing class.
    Signed-off-by: Ali Yussef <611252@student.inholland.nl>

--- src/main/java/nl/inholland/controller/AccountsApiController.java
+++ src/main/java/nl/inholland/controller/AccountsApiController.java
@@ -13,7 +13,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import javax.validation.constraints.*;
 import javax.validation.Valid;
 import javax.servlet.http.HttpServletRequest;
---
+++ src/test/java/nl/inholland/model/CurrentAccountTest.java
@@ -0,0 +1,36 @@
+package nl.inholland.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CurrentAccountTest {
+
+    private Account currentAccount;
+
+    @Before
+    public void setUp() {
+        currentAccount = new CurrentAccount();
+    }
+
+    @Test
+    public void buildIBAN() {
+    }
+
+    @Test
+    public void setBalance() {
+    }
+
+    @Test
+    public void setDateOfOpening() {
+    }
+
+    @Test
+    public void setAccountType() {
+    }
+
+    @Test
+    public void setAccountStatus() {
+    }
+}
