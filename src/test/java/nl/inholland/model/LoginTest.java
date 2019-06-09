1fa8bfafb799ae66ea45780edcee09245230b93c
Marianne Uhl <611034@student.inholland.nl>
2019-06-09 14:52:23 +0200

    made test class

---
+++ src/test/java/nl/inholland/model/LoginTest.java
@@ -0,0 +1,76 @@
+package nl.inholland.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class LoginTest {
+
+    private Login login;
+    private User user = new Customer("KL Poll", "Kile Poll",
+            User.SexEnum.fromValue("male"), "21.12.1972", new Address("Example",
+            111, "1111OP", "Nen", "The Netherlands"),
+            new Address("Example",111, "1111OP", "Nen",
+                    "The Netherlands"), "+3111132156", "example@gmail.com",
+            User.CommercialMessagesEnum.fromValue("by bankmail"), User.PreferredLanguageEnum.fromValue("Dutch"),
+            "Customer");
+
+    @Before
+    public void setUp(){
+        login = new Login(user.getEmailAddress(),user);
+    }
+
+    @Test
+    public void loginShouldNotBeNull(){
+        assertNotNull(login);
+    }
+
+    @Test
+    public void createLoginShouldNotBeNull(){
+        assertNotNull(login);
+    }
+    @Test
+    public void getUserShouldNotBeNull() {
+        assertNotNull(login.getUser());
+    }
+
+    @Test
+    public void setUserToUserWithNameRyanReynolds() {
+        User newUser = new Customer("R Reynolds", "Ryan",
+                User.SexEnum.fromValue("male"), "21.12.1972", new Address("Example",
+                111, "1111OP", "Nen", "The Netherlands"),
+                new Address("Example",111, "1111OP", "Nen",
+                        "The Netherlands"), "+3111132156", "example@gmail.com",
+                User.CommercialMessagesEnum.fromValue("by bankmail"), User.PreferredLanguageEnum.fromValue("Dutch"),
+                "Customer");
+        login.setUser(newUser);
+        assertEquals(newUser, login.getUser());
+
+    }
+
+    @Test
+    public void getUserNameShouldNotBeNull() {
+        assertNotNull(login.getUserName());
+    }
+
+    @Test
+    public void getPasswordShouldNotBeNull() {
+        assertNotNull(login.getPassword());
+    }
+
+    @Test
+    public void setPasswordToNewPasswordShouldChangeThePasswordToNewPassword(){
+        login.setPassword("NewPassword");
+        assertEquals("NewPassword", login.getPassword());
+    }
+
+    @Test
+    public void checkPasswordShouldGiveTrue(){
+        String password = "password";
+        login.setPassword(password);
+        assertEquals(true, login.checkPassword(password));
+    }
+
+}
