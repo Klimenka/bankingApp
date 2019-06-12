cb6613d30568d4d35d9a07e638687076f07c88d7
Nadia Klimenko <nmk2502@gmail.com>
2019-06-12 15:06:34 +0200

    forcing https use.

--- src/main/java/nl/inholland/configuration/SecurityConfiguration.java
+++ src/main/java/nl/inholland/configuration/SecurityConfiguration.java
@@ -32,6 +32,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.requiresChannel()
+                .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
+                .requiresSecure();
+        
         http.csrf().disable();
         http.authorizeRequests()
                 .antMatchers(HttpMethod.POST, "/accounts").hasAnyRole("Employee", "Owner")
