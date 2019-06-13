de0bbbc5091a826c0d124c9df8fc2a070fc00b4f
AliYussef <36632054+aliyussef@users.noreply.github.com>
2019-06-13 15:40:50 +0200

    testing
    Signed-off-by: Ali Yussef <611252@student.inholland.nl>

--- src/main/java/nl/inholland/configuration/SecurityConfiguration.java
+++ src/main/java/nl/inholland/configuration/SecurityConfiguration.java
@@ -38,6 +38,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
+        http.headers().frameOptions();
         http.authorizeRequests()
                 .antMatchers(HttpMethod.POST, "/accounts").hasAnyRole("Employee", "Owner")
                 .antMatchers(HttpMethod.POST, "/users").hasAnyRole("Employee", "Owner")
