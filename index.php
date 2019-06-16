b26365dc6da45d9f12a53d4906e1748d9aa67ec2
AliYussef <36632054+AliYussef@users.noreply.github.com>
2019-06-17 00:46:22 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Banking API</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="shortcut icon" href="css/hydra.ico" type="image/x-icon"/>
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">           
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+        <link rel="stylesheet" href="css/header3.css">
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>           
+        <script defer src="https://use.fontawesome.com/releases/v5.0.1/js/all.js"></script>  
+    </head>
+
+    <body>
+        <div class="haed-title">
+            <h1 class="display-1">Banking API</h1>
+            <p>by team Targaryen</p>
+            <button class="action btn btn-primary" onclick="window.open('https://banking-application-api.herokuapp.com')">Log in</button>	
+            <button class="action btn btn-Light" onclick="location.href = 'users.html'">Sign up</button>	
+        </div>
+
+        <header class="header" id="header1">
+            <div class="center">
+                <div class="caption animated bounceInDown">
+                    <h2 class="title display-3">Functionalities</h2>
+                    <p class="text">You can manage the following functionalities:</p>
+                    <button class="action btn btn-Light" onclick="location.href = 'accounts.html'">Bank Accounts</button>
+                    <button class="action btn btn-Light" onclick="location.href = 'users.html'">Users</button>
+                    <button class="action btn btn-Light" onclick="location.href = 'transaction.html'">Transactions</button>
+                </div>	
+            </div>
+
+        </header>
+
+        <div class="footer-more d-lg-flex justify-content-around">
+            <p><i class="fab fa-github-alt"></i> contribute on github <a href="https://github.com/Klimenka/bankingApp/tree/Submission-1">Banking API</a></p>
+        </div>
+
+    </body>
+</html>
